aacd476d77a19ec7db545c8d13188a0b0bb17830
natenadze1102 <natenadze1102@gmail.com>
2021-09-12 01:11:34 +0400

    complete arrays and functions / 2

---
+++ day-18/6-functions-part-2/02-tasks/2-homework/index-2.js
@@ -0,0 +1,48 @@
+/*Task 2
+
+Write your own implementation of function `compose`. Function is waiting for unlimited list of parameters. Each parameter has to be function. Each function is waiting for 1 parameter. Function `compose` returns another function with one parameter.
+
+Main idea of function `compose` is `compose(f, g)(x) = f(g(x))`
+
+Example:
+
+compose((str) => {
+    return str + 'c';
+}, (str) => {
+    return str + 'b';
+})('a'); // 'abc' */
+
+function compose(...functions) {
+  const arr = [];
+
+  for (f of functions) {
+    f();
+  }
+
+  return function (str) {
+    for (let f of functions) {
+      arr.push(f(str));
+    }
+
+    return arr
+      .join('')
+      .split('')
+      .filter((item, index, array) => array.indexOf(item) === index)
+      .sort()
+      .join('');
+  };
+}
+
+console.log(
+  compose(
+    (str) => {
+      return str + 'c';
+    },
+    (str) => {
+      return str + 'b';
+    },
+    (str) => {
+      return str + 'b';
+    }
+  )('a')
+);
