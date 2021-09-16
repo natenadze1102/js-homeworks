344d80ce761cba486ffb4c1cd0918837d39fe973
natenadze1102 <61623737+natenadze1102@users.noreply.github.com>
2021-09-16 11:31:13 +0400

    Update index-2.js
    fix task-2

--- day-18/1-arrays/index-2.js
+++ day-18/1-arrays/index-2.js
@@ -1,14 +1,10 @@
 /* Task 2
-
 Create function `reverse`, that gets array as parameter and returns array in opposite order.
-
 Function should contain next checks:
-
 - First parameter required and has to be only array
 - Throw error if passed empty array
 - Creating new array is not allowed
 - Using Array.reverse is not allowed
-
 javascript
 const arr = [3,2,1];
 reverse(arr); // [1,2,3]
@@ -20,13 +16,12 @@ function reverse(array) {
   try {
     if (array.length === 0) throw new Error('array should not be empty');
     else {
-      return array.sort((a, b) => {
-        if (typeof a === 'string' && typeof b === 'string') {
-          return b > a ? 1 : -1;
-        } else {
-          return b - a;
-        }
-      });
+      for (var i = 0; i <= Math.floor((arr.length - 1) / 2); i++) {
+        let el = arr[i];
+        arr[i] = arr[arr.length - 1 - i];
+        arr[arr.length - 1 - i] = el;
+    }
+    return arr;
     }
   } catch (e) {
     console.log(e);
