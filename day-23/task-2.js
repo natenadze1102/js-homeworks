047603db4ffc962a2c8fe5abe62618b6772ef7d6
natenadze1102 <natenadze1102@gmail.com>
2021-10-07 09:19:09 +0400

    complete 20%

--- day-23/task-2.js
+++ day-23/task-2.js
@@ -1,3 +1,23 @@
+/* Improve `DB` class from previous task.
+
+- Add a `find` method that will return an array of users that satisfy the condition passed as a parameter.
+- Generate an error if the `query` is not valid.
+- The fields `name` and` country` are looking for a 100% match.
+- The fields `age` and` salary` accept an object in which there must be either 2 parameters `min` and` max` or at least one of them.
+- Return an empty array if it was not possible to find users that satisfy the request object.
+
+```javascript
+const query = {
+    country: 'ua',
+    age: {
+        min: 21
+    },
+    salary: {
+        min: 300,
+        max: 600
+    }
+};
+const customers = db.find(query); // array of users */
 class DB {
   users = new Map();
   gen = function () {
@@ -54,6 +74,8 @@ class DB {
       throw new Error('non-existent or invalid id');
     return this.users.delete(id);
   }
+
+  find(query) {}
 }
 
 const db = new DB();
@@ -69,18 +91,62 @@ const person1 = {
   name: 'Gio', // required field with type string
   age: 22, // required field with type number
   country: 'uk', // required field with type string
-  salary: 5000, // required field with type number
+  salary: 450, // required field with type number
+};
+
+const person2 = {
+  name: 'Dato', // required field with type string
+  age: 29, // required field with type number
+  country: 'ge', // required field with type string
+  salary: 4000, // required field with type number
 };
 
 const id = db.create(person);
 const id1 = db.create(person1);
+const id2 = db.create(person1);
 
 const customer = db.read(id);
 const customer2 = db.read(id1);
+const customer3 = db.read(id1);
 
-db.update(id, { age: 25, name: 'Pat', country: 'ru' }); // id
-
-db.delete(id); // true
+//db.update(id, { age: 25, name: 'Pat', country: 'ru' }); // id
+// db.delete(id); // true
 
 const customers = db.readAll();
-console.log(customers);
+
+const query = {
+  country: 'ua',
+  age: {
+    min: 21,
+  },
+  salary: {
+    min: 300,
+    max: 600,
+  },
+};
+customers.filter((elem, index, array) => {
+  console.log(elem);
+  function iterate(obj) {
+    for (var prop in obj) {
+      if (obj.hasOwnProperty(prop)) {
+        if (elem.hasOwnProperty(prop)) console.log(prop + '!');
+        if (typeof obj[prop] == 'object') {
+          /* iterate(obj[prop]); */
+          return obj[prop].min < elem[prop];
+        } else {
+          /* console.log(` ${prop} ${obj[prop]}`); */
+        }
+      }
+    }
+  }
+
+  console.log(iterate(query));
+});
+
+/* let map = new Map(Object.entries(query));
+console.log(
+  map.entries().forEach(function (a, b, c) {
+    console.log(a);
+  })
+);
+ */
